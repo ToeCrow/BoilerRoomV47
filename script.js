ee5d1a4a0c1d0ea03e279588513067a13954181f
rebeccalindman <rebeccalindman@icloud.com>
2024-11-22 10:19:00 +0100

    search function

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-import {  } from "./search.js";
+// script.js
+import { fetchNews } from "./scripts/search.js";
 
-console.log(`API Key: ${API_KEY}`);
+/* console.log(`API Key: ${API_KEY}`); */
+
+const searchInput = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+const newsContainer = document.getElementById("news-container");
+
+// event listeners
+searchButton.addEventListener("click", () => {
+    
+    const url = fetchNews(); // returns url
+
+    displayNews(url); // todo - hämta nyheter, input variabel är url
+});
+
+
+
+export { searchInput, searchButton, newsContainer };
